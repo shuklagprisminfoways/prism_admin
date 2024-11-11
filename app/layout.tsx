feea7780d624ee7556e51f0d76f30a7f9d938817
Gyanesh Shukla <gyanesh12@prisminfoways.com>
2024-11-11 15:33:13 +0530

    Fix: Moved <Script /> outside of <Head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Head from 'next/head'
 import Script from 'next/script'
 import Footer from '@/components/Footer'
 import '../public/css/bootstrap.min.css'
@@ -24,17 +23,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <Script src="/js/jquery-3.3.1.min.js"></Script>
-        <Script src="/js/bootstrap.bundle.min.js"></Script>
-        <Script src="/js/Main.js"></Script>
-        <Script src="/js/owl.carousel.min.js"></Script>
-      </Head>
       <body className={inter.className}>
         <Navbar/>
         {children}
         <Footer/>
       </body>
+      <Script src="/js/jquery-3.3.1.min.js"></Script>
+        <Script src="/js/bootstrap.bundle.min.js"></Script>
+        <Script src="/js/Main.js"></Script>
+        <Script src="/js/owl.carousel.min.js"></Script>
     </html>
   )
 }
